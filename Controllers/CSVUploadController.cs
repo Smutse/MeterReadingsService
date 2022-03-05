ead0d1d4f12e75c0d04a88375d0b366a5ba7f304
chris smuts <chris.smuts@smartepay.com>
2022-03-05 19:45:57 +0000

    Reorganised Folders

--- Controllers/CSVUploadController.cs
+++ Controllers/CSVUploadController.cs
@@ -1,14 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Data;
-using System.Text;
-using System.Globalization;
-using System.Text.RegularExpressions;
-using Newtonsoft.Json;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace EnsekMeterReadingsService.Controllers
 {
